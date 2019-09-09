c51ad42daa3fdce0a7a710b3955a9e2fefd1e90b
Carlsen, Taylor <taylorcarlsen13@gmail.com>
2019-09-09 12:17:09 -0500

    Added listeners in the same view and multiple methods. Added options menu as well.

--- app/src/main/java/edu/fvtc/controldemo1/MainActivity.java
+++ app/src/main/java/edu/fvtc/controldemo1/MainActivity.java
@@ -3,7 +3,12 @@ package edu.fvtc.controldemo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,7 +19,7 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myDebug";
     Button btnDisplay;
@@ -27,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     EditText editText;
 
 
-    int[] imgIds = {-1, R.drawable.fish, R.drawable.lobster, R.drawable.shrimp, R.drawable.shrimp1};
+    int[] imgIds = {-1, R.drawable.fish, R.drawable.shrimp, R.drawable.lobster};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
 
+        /*cbCheckbox.setOnCheckedChangeListener(this); //method at the activity level to run code below
+        rbRadioButton.setOnCheckedChangeListener(this); //shares the same code as above*/
+
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,10 +64,100 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 textView.setTextColor(getColor(R.color.colorWhite));
             }
         });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("");
+            }
+        });
+
+        cbCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                int id = buttonView.getId();
+
+                if(isChecked){
+                    Log.d(TAG, id + " : Checked");
+                }
+                else{
+                    Log.d(TAG, id + " : Not checked");
+                }
+            }
+        });
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        Toast.makeText(this, "onCreateOptionsMenu",Toast.LENGTH_LONG).show();
+        return true;
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu); // puts xml into the menu
+
+        /*
+        menu.clear();
+
+        MenuItem menuItem = menu.add(Menu.NONE, 1, Menu.NONE, "Fish");
+        menuItem.setIcon(R.drawable.fish);
+
+        menuItem.setShowAsActionFlags(menuItem.SHOW_AS_ACTION_WITH_TEXT | menuItem.SHOW_AS_ACTION_ALWAYS);
+
+        MenuItem menuItem1 = menu.add(Menu.NONE, 2, Menu.NONE, "Shrimp");
+        menuItem.setIcon(R.drawable.shrimp);
+
+        MenuItem menuItem2 = menu.add(Menu.NONE, 3, Menu.NONE, "Lobster");
+        menuItem.setIcon(R.drawable.lobster);
+        */
+
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        try
+        {
+            // get the id of the menu item selected
+            int id = item.getItemId();
+
+            imgFood.setImageResource(imgIds[id]);
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void ShowName(View v)
+    {
+        String format = getString(R.string.format);
+        String name = String.format(format, TextUtils.htmlEncode(textView.getText().toString()));
+        Log.d(TAG, name);
+        Spanned spanned = Html.fromHtml(name, Html.FROM_HTML_MODE_LEGACY);
+        textView.setText(spanned);
+    }
+
+    /*
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        int id = buttonView.getId();
+
+        if(isChecked){
+            Log.d(TAG, id + " : Checked");
+        }
+        else{
+            Log.d(TAG, id + " : Not checked");
+        }
+    }
+*/
 }
+
